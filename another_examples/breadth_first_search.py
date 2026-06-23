af601c6e1e6df6e1a9cdbd720bc3ab7acef57f1d
João Marcelo Cruz Coelho <joao.ccoelho@upe.br>
2026-06-22 22:07:22 -0300

    Feat: resolução pelo algoritmo A* e correção da função heurística.

--- another_examples/breadth_first_search.py
+++ another_examples/breadth_first_search.py
@@ -8,24 +8,31 @@ def find_path(
 ) -> list[int]:
 
     if start == goal:
-        return [start]
+        return [start] # definimos um caso base para verificar se o goal = start
 
-    visitados = {start}
+    visitados = {start} # Usamos um conjunto (ou set) para rastrear os nós já visitados 
+    # com custo de busca otimizado O(1) -> tempo constante
 
-    fila = deque([(start, [start])])
+    fila = deque([(start, [start])]) # Essa fila serve para guardar o nó atual e o caminho
+    # até ele.
 
-    while fila:
-        intersection_atual, road = fila.popleft()
+    while fila: # Esse while serve para verificar cada intersection e seus vizinhos
+        # até chegar no goal
+        intersection_atual, road_atual = fila.popleft() # pega o último elemento da esquerda
 
-        if intersection_atual == goal:
-            return road
+        if intersection_atual == goal: # se encontrarmos o goal retorna o caminho atual
+            return road_atual
 
         if intersection_atual not in city_map.roads:
-            continue
+            continue # se a intersection não existir ele apenas continua
 
-        for vizinho in city_map.roads[intersection_atual]:
+        for vizinho in city_map.roads[intersection_atual]: # Esse for analisa os vizinhos
+            # de cada intersection
             if vizinho not in visitados:
                 visitados.add(vizinho)
-                fila.append((vizinho, road + [vizinho]))
+                fila.append((vizinho, road_atual + [vizinho]))
+            # Esse if serve para um vizinho se tornar a próxima intersection a ser vista
 
-    return []
+    return [] # Caso não houver solução retorna uma lista vazia pois não existe caminho
+
+    
